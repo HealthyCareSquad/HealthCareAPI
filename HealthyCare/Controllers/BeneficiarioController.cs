c48113255ef6b6fa725284a6ae74e6977f580135
Karlessandra Lima <karlessandralima@gmail.com>
2022-12-19 20:59:47 -0300

    Add project files.

---
+++ HealthyCare/Controllers/BeneficiarioController.cs
@@ -0,0 +1,101 @@
+﻿using HealthyCare.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyCare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeneficiarioController : Controller
+    {
+        private readonly BeneficiarioRepository beneficiarioRepository;
+        public BeneficiarioController()
+        {
+            beneficiarioRepository = new BeneficiarioRepository();
+        }
+
+        [HttpGet]
+        [Route("/Consultar")]
+        public IActionResult Consultar()
+        {
+            try
+            {
+                var listaBeneficiario = beneficiarioRepository.Listar();
+                return Ok(listaBeneficiario);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("/Consultar/{id}")]
+        public ActionResult Consultar(int id)
+        {
+            try
+            {
+                var beneficiario = beneficiarioRepository.Consultar(id);
+                if (beneficiario.IdBeneficiario != 0)
+                {
+                    return Ok(beneficiario);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        // Anotação de uso do Verb HTTP Post
+        [HttpPost]
+        [Route("/Cadastrar")]
+        public ActionResult Cadastrar(Modelos.Beneficiario beneficiario)
+        {
+            try
+            {
+                beneficiarioRepository.Inserir(beneficiario);
+                return Ok(beneficiario);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut]
+        [Route("/Editar")]
+        public ActionResult Editar(Modelos.Beneficiario beneficiario)
+        {
+            try
+            {
+                beneficiarioRepository.Alterar(beneficiario);
+                return Ok(beneficiario);
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        [Route("/Excluir/{id}")]
+        public ActionResult Excluir(int id)
+        {
+
+            try
+            {
+                beneficiarioRepository.Excluir(id);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ HealthyCare/Repository/BeneficiarioRepository.cs
@@ -0,0 +1,54 @@
+﻿using HealthyCare.Modelos;
+using HealthyCare.Repository.Context;
+
+namespace HealthyCare.Repository
+{
+    public class BeneficiarioRepository
+    {
+        private readonly DataBaseContext context;
+
+        public BeneficiarioRepository()
+        {
+            // Criando um instância da classe de contexto do EntityFramework
+            context = new DataBaseContext();
+        }
+
+        public IList<Beneficiario> Listar()
+        {
+
+            return context.Beneficiarios.ToList();
+        }
+
+        public Beneficiario Consultar(int id)
+        {
+            return context.Beneficiarios.Find(id);
+        }
+
+
+        public void Inserir(Beneficiario beneficiario)
+        {
+            context.Beneficiarios.Add(beneficiario);
+            context.SaveChanges();
+        }
+
+        public void Alterar(Beneficiario beneficiario)
+        {
+            context.Beneficiarios.Update(beneficiario);
+            context.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            // Criar um tipo produto apenas com o Id
+            var beneficiario = new Beneficiario()
+            {
+                IdBeneficiario = id
+            };
+
+            context.Beneficiarios.Remove(beneficiario);
+            context.SaveChanges();
+        }
+    }
+}
+    
+
